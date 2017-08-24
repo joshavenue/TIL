46d744ac84faf6d4d6053a03e327576cac2d1139
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-25 01:03:03 +0800

    Create case_switch.js

---
+++ case_switch.js
@@ -0,0 +1,16 @@
+var moonPhase = 'haha';
+
+switch (moonPhase){
+  case 'full':
+    console.log('Howll');
+    break;
+  case 'mostly full':
+    console.log('arm are getting hairy');
+    break;
+  case 'mostly new':
+    console.log('back on two feet');
+    break;
+  default:
+    console.log('invalid moon phase');
+    break;
+                 }
