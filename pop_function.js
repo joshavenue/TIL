3a86f2df72220ed38b4b03bd1a75e61e9e7d4e38
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-25 11:24:07 +0800

    Create pop_function.js

---
+++ pop_function.js
@@ -0,0 +1,10 @@
+var bucketList = ['learn to juggle', 'take a falconry class', 'climb Mt Everest'];
+var listItem = bucketList[3];
+console.log(listItem);
+console.log(bucketList.length);
+
+bucketList.push('dog', 'cat');
+console.log(bucketList);
+
+bucketList.pop();  // Adding a pop() function will delete the last array from the variable //
+console.log(bucketList);
