4165c819c16d1ef6807ad329df94daeec46477a7
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-25 11:22:05 +0800

    Create push_function.js

---
+++ push_function.js
@@ -0,0 +1,9 @@
+var bucketList = ['learn to juggle', 'take a falconry class', 'climb Mt Everest'];
+var listItem = bucketList[3];
+console.log(listItem);
+console.log(bucketList.length);
+
+bucketList.push('dog', 'cat');
+console.log(bucketList);
+
+// You can add stuff into arrays by using variable.push(whatever it is), .push allow you to add new arrays into array //
